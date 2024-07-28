034fb3cbee33ed7b332124fdc507878cefd37422
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2024-07-28 19:21:35 -0300

    feat: create Notification Service

---
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDTO;
+
+@Service
+public class NotificationService {
+    
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception{
+
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK) )
+            throw new Exception("Serviço de notificação está fora do ar");
+
+        System.out.println("notificacao enviada para o usuario");
+    }
+}
